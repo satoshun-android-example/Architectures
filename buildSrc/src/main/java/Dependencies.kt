5b8c12b1c47633d663c4eba6e5ead9afbeac1809
SatoShun <shun.sato1@gmail.com>
2018-10-14 07:01:04 +0000

    mobius first

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,57 +1,60 @@
 object Vers {
-  val compile_sdk = 28
-  val min_sdk = 26
-  val target_sdk = 28
+  const val compile_sdk = 28
+  const val min_sdk = 26
+  const val target_sdk = 28
 
-  val kotlin = "1.3.0-rc-57"
-  val couroutine = "0.30.0-eap13"
+  const val kotlin = "1.3.0-rc-57"
+  const val coroutine = "0.30.0-eap13"
 }
 
 object Libs {
-  val android_plugin = "com.android.tools.build:gradle:3.2.1"
-  val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
-  val versions_plugin = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
+  const val android_plugin = "com.android.tools.build:gradle:3.2.1"
+  const val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
+  const val versions_plugin = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
 
-  val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
-  val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vers.couroutine}"
-  val ui_coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Vers.couroutine}"
+  const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
+  const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vers.coroutine}"
+  const val ui_coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Vers.coroutine}"
 
-  val ktx = "androidx.core:core-ktx:1.0.0"
+  const val ktx = "androidx.core:core-ktx:1.0.0"
 
-  val appcompat = "androidx.appcompat:appcompat:1.0.0"
-  val recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
-  val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
-  val cardview = "androidx.cardview:cardview:1.0.0"
+  const val appcompat = "androidx.appcompat:appcompat:1.0.0"
+  const val recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
+  const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
+  const val cardview = "androidx.cardview:cardview:1.0.0"
 
-  val material = "com.google.android.material:material:1.0.0"
-  val coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
-  val paging = "androidx.paging:paging-runtime:2.0.0-rc01"
-  val navigation = "android.arch.navigation:navigation-fragment-ktx:1.0.0"
-  val navigation_ui = "android.arch.navigation:navigation-ui-ktx:1.0.0"
+  const val material = "com.google.android.material:material:1.0.0"
+  const val coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
+  const val paging = "androidx.paging:paging-runtime:2.0.0-rc01"
+  const val navigation = "android.arch.navigation:navigation-fragment-ktx:1.0.0"
+  const val navigation_ui = "android.arch.navigation:navigation-ui-ktx:1.0.0"
 
-  val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
-  val livedata = "androidx.lifecycle:lifecycle-livedata:2.0.0"
-  val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:2.0.0"
+  const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+  const val livedata = "androidx.lifecycle:lifecycle-livedata:2.0.0"
+  const val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:2.0.0"
 
-  val emoji = "androidx.emoji:emoji:1.0.0"
-  val emoji_compat = "androidx.emoji:emoji-appcompat:1.0.0"
-  val emoji_bundled = "androidx.emoji:emoji-bundled:1.0.0"
+  const val emoji = "androidx.emoji:emoji:1.0.0"
+  const val emoji_compat = "androidx.emoji:emoji-appcompat:1.0.0"
+  const val emoji_bundled = "androidx.emoji:emoji-bundled:1.0.0"
 
-  val coroutine_binding = "com.github.satoshun.coroutinebinding:coroutinebinding:0.1.1"
+  const val coroutine_binding = "com.github.satoshun.coroutinebinding:coroutinebinding:0.1.1"
 
-  val multidex = "com.android.support:multidex:1.0.3"
+  const val multidex = "com.android.support:multidex:1.0.3"
 
-  val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
-  val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
-  val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
-  val rxbinding = "com.jakewharton.rxbinding2:rxbinding:2.2.0"
+  const val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
+  const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
+  const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
+  const val rxbinding = "com.jakewharton.rxbinding2:rxbinding:2.2.0"
 
-  val rxredux = "com.freeletics.rxredux:rxredux:1.0.1"
-  val mvrx = "com.airbnb.android:mvrx:0.5.0"
+  const val rxredux = "com.freeletics.rxredux:rxredux:1.0.1"
+  const val mvrx = "com.airbnb.android:mvrx:0.5.0"
+  const val mobius = "com.spotify.mobius:mobius-core:1.2.0"
+  const val mobius_rx2 = "com.spotify.mobius:mobius-rx2:1.2.0"
+  const val mobius_android = "com.spotify.mobius:mobius-android:1.2.0"
 
-  val junit = "junit:junit:4.12"
-  val truth = "com.google.truth:truth:0.39"
-  val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
-  val test_runner = "androidx.test:runner:1.1.0-alpha4"
-  val espresso = "androidx.test.espresso:espresso-core:3.1.0-alpha4"
+  const val junit = "junit:junit:4.12"
+  const val truth = "com.google.truth:truth:0.39"
+  const val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
+  const val test_runner = "androidx.test:runner:1.1.0-alpha4"
+  const val espresso = "androidx.test.espresso:espresso-core:3.1.0-alpha4"
 }
---
+++ mobius/src/main/java/com/github/satoshun/example/architectures/mobius/MobiusActivity.kt
@@ -0,0 +1,26 @@
+package com.github.satoshun.example.architectures.mobius
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.android.Main
+import kotlin.coroutines.CoroutineContext
+
+class MobiusActivity : AppCompatActivity(),
+    CoroutineScope {
+
+  private val job = Job()
+  override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    job.cancel()
+  }
+}
