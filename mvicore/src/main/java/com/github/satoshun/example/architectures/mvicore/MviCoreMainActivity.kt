d352f5d1fdb05f0b6097688d6b8767515ad3ad9d
SatoShun <shun.sato1@gmail.com>
2018-10-15 00:45:08 +0000

    mvi first

--- mvicore/src/main/java/com/github/satoshun/example/architectures/mvicore/MviCoreMainActivity.kt
+++ mvicore/src/main/java/com/github/satoshun/example/architectures/mvicore/MviCoreMainActivity.kt
@@ -1,6 +1,8 @@
 package com.github.satoshun.example.architectures.mvicore
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.badoo.mvicore.android.AndroidBindings
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -13,8 +15,24 @@ class MviCoreMainActivity : AppCompatActivity(),
   private val job = Job()
   override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main
 
+  private lateinit var bindings: MainActivityBindings
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act)
+
+    bindings.setup(this)
+  }
+
   override fun onDestroy() {
     super.onDestroy()
     job.cancel()
+  }
+}
+
+class MainActivityBindings(
+  view: MviCoreMainActivity
+) : AndroidBindings<MviCoreMainActivity>(view) {
+  override fun setup(view: MviCoreMainActivity) {
   }
 }
